990f0e5f9c9d0a3b0b3d6b7121fd7e70c8a6144a
GrundyDenis <mburugudenis7@gmail.com>
2022-09-10 22:04:22 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 /**
-* main - print the string in the put function
-*
-* Description: using the main function
-* return: 0
+* Description - Prints out to string to stdout.
+* Return : 0 if success.
 */
 int main(void)
 {
